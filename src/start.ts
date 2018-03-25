90b6b064b0225eba63dda93de72b539b7a02481f
Federico Marcos <marcosfede@gmail.com>
2018-03-25 00:55:00 -0300

    nos pusimos la gorra

--- src/start.ts
+++ src/start.ts
@@ -1,39 +1,13 @@
 import * as compression from 'compression'
+import * as RateLimit from 'express-rate-limit'
+import * as depthLimit from 'graphql-depth-limit'
 // import { ApolloEngine } from 'apollo-engine'
 import {importSchema} from 'graphql-import'
 import {GraphQLServer} from 'graphql-yoga'
 import 'reflect-metadata'
-import {createConnection, getRepository} from 'typeorm'
-
-import {Film} from './entity'
+import {createConnection} from 'typeorm'
 import createLoaders from './loaders'
 import resolvers from './resolvers'
-
-class DefaultDict {
-  private map: Map<number, any[]>
-  constructor() {
-    this.map = new Map()
-  }
-  public add(to, id) {
-    let current = this.map.get(to)
-    if (!current) {
-      current = []
-    }
-    current.push(id)
-    this.map.set(to, current)
-  }
-
-  public get(id) {
-    return this.map.get(id)
-  }
-
-  public toString() {
-    for (const key of this.map.keys()) {
-      const valueIds = this.map.get(key).map(value => value.id)
-      console.log(key, valueIds)
-    }
-  }
-}
 
 createConnection()
   .then(async connection => {
@@ -45,9 +19,21 @@ createConnection()
       },
     })
     server.express.use(compression())
-
+    const limiter = new RateLimit({
+      windowMs: 24 * 60 * 60 * 1000, // 24 hours
+      max: 2000, // limit each IP to 2000 requests per windowMs
+      delayMs: 0 // disable delaying - full speed until the max limit is reached
+    })
+    // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+    // server.express.enable('trust proxy')
+    server.express.use(limiter)
     server
-      .start({tracing: true, cacheControl: true})
+      .start({
+        tracing: true,
+        cacheControl: true,
+        port: 4000,
+        validationRules: [depthLimit(10)]
+      })
       .then(() => console.log(`Server started`))
       .then(() => console.log(' ****************************** '))
       .catch(e => console.error(e))
