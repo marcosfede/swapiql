d83bffef43a2b77cbbe60b3e1593c148a5d2b1b2
Federico Marcos <marcosfede@gmail.com>
2018-03-25 22:11:09 -0300

    moved json to folder

--- server/fixtures/index.ts
+++ server/fixtures/index.ts
@@ -1,10 +1,10 @@
-import * as films from './films.json'
-import * as people from './people.json'
-import * as planets from './planets.json'
-import * as species from './species.json'
-import * as starships from './starships.json'
-import * as transport from './transport.json'
-import * as vehicles from './vehicles.json'
+import * as films from './json/films.json'
+import * as people from './json/people.json'
+import * as planets from './json/planets.json'
+import * as species from './json/species.json'
+import * as starships from './json/starships.json'
+import * as transport from './json/transport.json'
+import * as vehicles from './json/vehicles.json'
 
 export {
   films,
