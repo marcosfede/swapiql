0e292861d8853bdca77a2a3be1210b4d813006b1
Matías Pierobon <matias.pierobonm@gmail.com>
2018-04-27 02:14:02 -0300

    compose base index page

--- client-next/pages/index.tsx
+++ client-next/pages/index.tsx
@@ -1,20 +1,41 @@
-import Layout from '../components/Layout'
-import globalStyles from '../styles/global.styles'
 import Link from 'next/link'
+import Layout from '../components/Layout'
+import Button from '../components/Button'
 
 export default () => (
   <Layout>
-    <div className="flex flex-row h-100 w-100">
-      <div className="w-50"></div>
-      <div className="w-50 flex flex-column justify-center items-center">
-        <div className="salmon f1 helvetica">Choose your side</div>
+      <div className="pa4 order-1 flex w-50-l flex-column justify-center items-start-l items-center">
+        <img className="darth-vader" src="/static/darth_vader_PNG29.png" />
+      </div>
+      <div className="pa4-ns flex order-0 order-1-l flex-column w-50-l justify-center items-start-l items-center">
+        <h1 className="text-primary f1">Choose your side</h1>
         <div className="flex flex-row mv3">
-          <Link prefetch href="/"><div className="b--grey ba f3 pa1 mh3 w4 flex justify-center gray br2">Explore</div></Link>
-          <Link href='/playground'><div className="b--salmon ba black f3 pa1 mh3 w4 flex justify-center bg--salmon br2">Playground</div></Link>
+          <Link prefetch href="/">
+            <a>
+              <Button dashed className="w4 mr3">
+                Explore
+              </Button>
+            </a>
+          </Link>
+          <Link prefetch href="/playground">
+            <a>
+              <Button primary className="w4">
+                Playground
+              </Button>
+            </a>
+          </Link>
         </div>
       </div>
-    </div>
-
-    <style global jsx>{globalStyles}</style>
+    <style jsx>{`
+      h1 {
+        font-weight: 100;
+        margin-top: 0;
+      }
+      .darth-vader {
+        border-radius: 0 0 50%;
+        max-width: 100%;
+        opacity: 0.25;
+      }
+    `}</style>
   </Layout>
 )
