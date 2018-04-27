0e107f6db4d6777af110d48416910e9ac5e422dd
Matías Pierobon <matias.pierobonm@gmail.com>
2018-04-27 02:13:34 -0300

    move layout to navbar and compose base structure

---
+++ client-next/components/Layout/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Header from '../Header'
+import globalStyles from '../../styles/global.styles'
+import layoutStyles from './styles'
+
+export default ({ children }) => (
+  <div className="root mw9 ph3 ph5-l flex flex-column">
+    <Head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <title>SWAPI-QL</title>
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+      />
+      <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" />
+    </Head>
+    <Header />
+    <div className="flex flex-column flex-row-l flex-auto">{children}</div>
+
+    <style global jsx>
+      {globalStyles}
+    </style>
+    <style jsx>{layoutStyles}</style>
+  </div>
+)
---
+++ client-next/components/Layout/styles.ts
@@ -0,0 +1,12 @@
+import css from 'styled-jsx/css'
+
+export default css`
+  .root {
+    min-height: 100vh;
+    width: 100vw;
+    background-color: var(--main-bg-color);
+    color: var(--main-fg-color);
+    font-family: 'Lato';
+    margin: 0 auto;
+  }
+`
