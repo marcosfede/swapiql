a1623e1e17711ded61913a0e042f1f31fa002737
Matías Pierobon <matias.pierobonm@gmail.com>
2018-04-27 02:11:53 -0300

    base global css for colors

--- client-next/styles/global.styles.ts
+++ client-next/styles/global.styles.ts
@@ -1,22 +1,44 @@
 import css from 'styled-jsx/css'
 
 export default css`
-  body {
-    background: #191919;
-  }
-  html,
-  body,
-  #__next {
-    height: 100%;
+  :root {
+    --main-bg-color: #1a1a1a;
+    --main-fg-color: #b3b3b3;
+    --primary-color: #d95468;
+    --secondary-color: #499bfc;
+    --secondary-fg-color: #a0b3c6;
   }
 
-  .grey {
-    --grey: #868686;
-    color: var(--grey);
+  @font-face {
+    font-family: 'starjedi';
+    src: url('/static/Starjedi.ttf');
   }
-  .salmon {
-    --salmon: #c45962;
-    color: var(--salmon);
+
+  html,
+  body {
+    background-color: var(--main-bg-color);
   }
-  .bg--salmon { background-color: #c45962}
+
+  a {
+    color: var(--primary-color);
+    text-decoration: none;
+  }
+
+  .text-primary {
+    color: var(--primary-color);
+  }
+
+  .text-secondary {
+    color: var(--secondary-color) !important;
+  }
+
+  .bg-primary {
+    background-color: var(--primary-color);
+    color: var(--main-bg-color);
+  }
+
+  .bg-secondary {
+    background-color: var(--primary-color);
+    color: var(--main-bg-color);
+  }
 `
