b242e9bbd9e3035e57e886572abef1da91f90dcf
Federico Marcos <federico.marcos@celerative.com>
2018-11-25 20:50:28 -0300

    api url

--- server/src/start.ts
+++ server/src/start.ts
@@ -40,8 +40,8 @@ createConnection()
         tracing: true,
         cacheControl: true,
         port: 4000,
-        endpoint: isProd ? '/' : '/',
-        playground: isProd ? '/playground/' : '/playground/',
+        endpoint: isProd ? '/api/' : '/',
+        playground: isProd ? '/api/playground/' : '/playground/',
         validationRules: [depthLimit(10)],
       })
       .then(() => console.log(`Server started`))
